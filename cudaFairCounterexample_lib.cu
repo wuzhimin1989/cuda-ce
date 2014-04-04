b0fe7ecedd0a40b59282f607419b2312e07501e9
wuzhimin1989 <wzm198965@gmail.com>
2014-04-04 13:03:07 +0800

    2014.4.1
    new architecture version

--- cudaFairCounterexample_lib.cu
+++ cudaFairCounterexample_lib.cu
@@ -171,7 +171,7 @@ public:
 	GQueue()
 	{
 		blockcount = 0;
-		backupblockcount = 0;
+		//backupblockcount = 0;
 	}
 	~GQueue(){;}
 };
@@ -282,11 +282,13 @@ __global__ void BSearchIfreach(bool * theresult, int * searchlist, int size, int
 	}
 }
 
-__global__ void GPath(int startid, int * scc, int ** outgoing, int * path2scc)
+__global__ void GPath(int startid, int * scc, int ** outgoing, int * path2scc)  //for optimization, if outgoing is not very big, it can be stored in the specific memory in kepler
 {
 	int threadindex = blockIdx.x * blockDim.x + threadIdx.x;
+	int inblocktid = threadindex;
+	int Squeueindex,Squeueposindex;
 
-	extern __shared__ Queue<Pathnode> Init_S_queue[8];
+	extern __shared__ Queue<Pathnode> Init_S_queue[4][32];
 	extern __shared__ int queuesize;
 	extern __shared__ bool ifexpand;
 	extern __shared__ bool ifSccReach;
@@ -295,14 +297,17 @@ __global__ void GPath(int startid, int * scc, int ** outgoing, int * path2scc)
 
 	Pathnode * tmpnode;
 
-	if(threadindex == 0)
+	Squeueindex = (inblocktid/32+1)%3;
+	Squeueposindex = inblocktid % 31;
+
+	if(inblocktid == 0)
 	{
 		tmpnode = new Pathnode(startid);
 		tmpnode->tmppath[tmpnode->queueindex] = new queue<int>();
 		tmpnode->tmppath[tmpnode->queueindex]->push(startid);
 
 		tmpnode->queueindex ++;
-		Init_S_queue[threadindex % 7].push(*tmpnode);
+		Init_S_queue[Squeueindex][Squeueposindex].push(*tmpnode);
 		queuesize = 1;
 		ifexpand = false;
 		ifSccReach = false;
@@ -314,7 +319,7 @@ __global__ void GPath(int startid, int * scc, int ** outgoing, int * path2scc)
 	do{
 		if(threadindex < queuesize)
 		{
-			Pathnode * peeknode = &Init_S_queue[threadindex % 7].front();
+			Pathnode * peeknode = &Init_S_queue[Squeueindex][Squeueposindex].front();
 			
 			if(peeknode)
 			{
@@ -354,10 +359,10 @@ __global__ void GPath(int startid, int * scc, int ** outgoing, int * path2scc)
 						pathcount ++;
 					}
 					
-					Init_S_queue[threadindex % 7].push(*tmpnode);
+					Init_S_queue[Squeueindex][Squeueposindex].push(*tmpnode);
 					succ_num++;
 				}
-				Init_S_queue[threadindex % 7].pop();
+				Init_S_queue[Squeueindex][Squeueposindex].pop();
 			}			
 		}
 		if(threadindex == 0)
@@ -365,10 +370,10 @@ __global__ void GPath(int startid, int * scc, int ** outgoing, int * path2scc)
 
 		cudaThreadSynchronize();
 
-		if(threadindex== 0)
+		if(inblocktid == 0)
 		{
-			for(int i = 0; i < 8; i++)
-				queuesize += Init_S_queue[i].count();
+			for(int i = 0; i < 32; i++)
+				queuesize += Init_S_queue[Squeueindex][i].count();
 			if(queuesize > INITIAL_T)
 				ifexpand = true;
 		}
@@ -378,14 +383,16 @@ __global__ void GPath(int startid, int * scc, int ** outgoing, int * path2scc)
 	int expandedtasksize = 0;
 	int childbsize = 0;
 
-	if(!ifSccReach && threadindex == 0)
+	if(!ifSccReach && inblocktid == 0)
 	{
 		/*!!!important!!!FOR THIS PART, how many task to put in each block is very important, in order to decrease the time to call child, 
 		* maybe the thread in each block should be more than the task, this can be verified in experiments*/
-
-		for(int i=0; i<8; i++)                   
+		for(int j=0; j<4; j++)
 		{
-			expandedtasksize += Init_S_queue[i].count();
+			for(int i=0; i<32; i++)                   
+			{
+				expandedtasksize += Init_S_queue[j][i].count();
+			}
 		}
 		childbsize = expandedtasksize / WARP_T + 1;
 		G_Queue.G_queue = new Pathnode * [childbsize];
@@ -394,20 +401,26 @@ __global__ void GPath(int startid, int * scc, int ** outgoing, int * path2scc)
 
 		for(int j=0; j<childbsize; j++)
 		{
-			G_Queue.G_queue[j] = new __device__ Pathnode[TOTALSIZE-SCCSIZE - WARP_T];
+			if(TASKTYPE == 1)
+				G_Queue.G_queue[j] = new __device__ Pathnode[TOTALSIZE-SCCSIZE - WARP_T]; //queue stored in Global Queue
+			else
+				G_Queue.G_queue[j] = new __device__ Pathnode[SCCSIZE - WARP_T];
 			G_Queue.G_queue_size[j] = 0;
 		}
 		
 		int tmpcount = 0;
-		for(int j = 0; j < 8; j++)
+		for(int i = 0; i < 4; i++)
 		{
-			for(int m = 0; m < Init_S_queue[j].count() - INITIAL_T/8; m++)
+			for(int j = 0; j < 32; j++)
 			{
-				G_Queue.G_queue[tmpcount][G_Queue.G_queue_size[tmpcount]] = Init_S_queue[j].front();    //not sure about if the memory copy will work,need confirm.
-				G_Queue.G_queue_size[tmpcount]++;
-				Init_S_queue[j].pop();
-				tmpcount++;
-				tmpcount=tmpcount%(childbsize-1);
+				for(int m = 0; m < Init_S_queue[i][j].count(); m++)
+				{
+					G_Queue.G_queue[tmpcount][G_Queue.G_queue_size[tmpcount]] = Init_S_queue[i][j].front();    //not sure about if the memory copy will work,need confirm.
+					G_Queue.G_queue_size[tmpcount]++;
+					Init_S_queue[i][j].pop();
+					tmpcount++;
+					tmpcount=tmpcount%(childbsize-1);
+				}
 			}
 		}
 	}
@@ -422,7 +435,7 @@ __global__ void GPath(int startid, int * scc, int ** outgoing, int * path2scc)
 		bool ifneedsyn = false;    
 
 		//rearrange tasks
-		if(threadindex == 0)
+		if(inblocktid == 0)
 		{
 			if(expandtime > 1)  
 			{
@@ -437,31 +450,31 @@ __global__ void GPath(int startid, int * scc, int ** outgoing, int * path2scc)
 			expandedtasksize = queuesize;
 		}
 
-		if(threadindex < childbsize)
+		if(inblocktid < childbsize)
 		{
-			int beginindex = P_taskd_index[threadindex];
+			int beginindex = P_taskd_index[inblocktid];
 			/*for(int i=0; i<G_Queue.G_queue_size[threadindex]; i++)
 			{
 			P_G_sequence_index[i+beginindex] = &G_Queue.G_queue[threadindex][i];
 			}*/
 			//if array are sequential stored then:
-			P_G_sequence_index[beginindex] = G_Queue.G_queue[threadindex];
+			P_G_sequence_index[beginindex] = G_Queue.G_queue[inblocktid];
 		}
-		if(childbsize > BLOCK_T)
+		if(childbsize > BLOCK_T)    //adjust the map between virtual P_G_Quene to G_queue; 
 		{
 			int beginindex, leftsize;
 			for(int j = 0; j<childbsize/BLOCK_T; j++)
 			{
-				beginindex = P_taskd_index[j*BLOCK_T + threadindex];
-				P_G_sequence_index[beginindex] = G_Queue.G_queue[j*BLOCK_T + threadindex];
+				beginindex = P_taskd_index[j*BLOCK_T + inblocktid];
+				P_G_sequence_index[beginindex] = G_Queue.G_queue[j*BLOCK_T + inblocktid];
 			}
 			leftsize = childbsize % BLOCK_T;
-			if(threadindex < leftsize)
-				P_G_sequence_index[P_taskd_index[(childbsize/BLOCK_T)*BLOCK_T + threadindex]] = G_Queue.G_queue[(childbsize/BLOCK_T)*BLOCK_T + threadindex];
+			if(inblocktid < leftsize)
+				P_G_sequence_index[P_taskd_index[(childbsize/BLOCK_T)*BLOCK_T + inblocktid]] = G_Queue.G_queue[(childbsize/BLOCK_T)*BLOCK_T + inblocktid];
 		}
 		////////////////////////////////
 	
-		if(threadindex == 0)
+		if(inblocktid == 0)
 		{
 			int averagetask = expandedtasksize/childbsize + 1;
 			if(averagetask > WARP_T)
@@ -501,12 +514,13 @@ __global__ void GPath(int startid, int * scc, int ** outgoing, int * path2scc)
 	}	
 }
 
-__global__ void ChildPath(Pathnode ** G_sequence_Queue, int * taskindex, int * p2scc, int * scc, int ** outgoing)   //dynamic parallel in cuda
+__global__ void ChildPath(Pathnode ** G_sequence_Queue, int * taskindex, int * p2scc, int * scc, int ** outgoing)   //dynamic parallel in cuda, all static data could be stored in specific storage of Kepler
 {
 	int inblocktindex = threadIdx.x;
 	int globalthreadindex = blockDim.x * blockIdx.x + threadIdx.x;
+	int Squeueindex, Squeueposindex;
 
-	extern __shared__ Queue<Pathnode> Child_Init_S_queue[8];
+	extern __shared__ Queue<Pathnode> Child_Init_S_queue[4][32];
 	extern __shared__ int queuesize;
 	extern __shared__ bool ifSccReach;
 	extern __shared__ bool iffinish;
@@ -536,6 +550,8 @@ __global__ void ChildPath(Pathnode ** G_sequence_Queue, int * taskindex, int * p
 	else
 		__gpu_blocks_tree_syn(goalVal++, Arrayin, Arrayout);
 	
+	Squeueindex = inblocktindex/32;
+	Squeueposindex = inblocktindex % 31;
 	while(!G_ifsccReach && !Child_need_back2parent)
 	{
 		//copy data from global memory to shared memory
@@ -543,14 +559,14 @@ __global__ void ChildPath(Pathnode ** G_sequence_Queue, int * taskindex, int * p
 
 		if(inblocktindex < duration)
 		{
-			for(int i=0; i<duration/8; i++)
+			for(int i=0; i<duration/blockDim.x; i++)
 			{
-				Child_Init_S_queue[inblocktindex % 7].push(*(G_sequence_Queue)[taskindex[blockIdx.x]+ i*duration + inblocktindex]);
+				Child_Init_S_queue[Squeueindex][Squeueposindex].push(*(G_sequence_Queue)[taskindex[blockIdx.x]+ i * blockDim.x + inblocktindex]);
 			}
 		}
-		if(inblocktindex < duration - (duration/8)*8)
+		if(inblocktindex < duration - (duration/blockDim.x)*blockDim.x)
 		{
-			Child_Init_S_queue[inblocktindex % 7].push(*(G_sequence_Queue)[taskindex[blockIdx.x] + (duration/8)*8 + inblocktindex]);
+			Child_Init_S_queue[Squeueindex][Squeueposindex].push(*(G_sequence_Queue)[taskindex[blockIdx.x] + (duration/blockDim.x)*blockDim.x + inblocktindex]);
 		}
 		cudaThreadSynchronize();
 
@@ -559,7 +575,7 @@ __global__ void ChildPath(Pathnode ** G_sequence_Queue, int * taskindex, int * p
 		//////////////////////////////////////////////////
 		if(inblocktindex < queuesize)
 		{
-			Childpeeknode = &Child_Init_S_queue[inblocktindex % 7].front();
+			Childpeeknode = &Child_Init_S_queue[Squeueindex][Squeueposindex].front();
 			if(Childpeeknode)
 			{
 				int succ_num = 0;
@@ -601,11 +617,11 @@ __global__ void ChildPath(Pathnode ** G_sequence_Queue, int * taskindex, int * p
 						pathcount ++;
 					}
 
-					Child_Init_S_queue[inblocktindex % 7].push(*tmpnode);
+					Child_Init_S_queue[Squeueindex][Squeueposindex].push(*tmpnode);
 					succ_num++;
 				}
 
-				Child_Init_S_queue[inblocktindex % 7].pop();				
+				Child_Init_S_queue[Squeueindex][Squeueposindex].pop();				
 			}
 		}
 
@@ -617,10 +633,13 @@ __global__ void ChildPath(Pathnode ** G_sequence_Queue, int * taskindex, int * p
 		int cpbackindex[8];
 		if(inblocktindex == 0)
 		{
-			for(int i = 0; i < 8; i++)
+			for(int j = 0; j < 4; j++)
 			{
-				queuesize += Child_Init_S_queue[i].count();
-				cpbackindex[i]=queuesize;
+				for(int i = 0; i < 32; i++)
+				{
+					queuesize += Child_Init_S_queue[j][i].count();
+					cpbackindex[i]=queuesize;
+				}
 			}
 			if(queuesize > blockDim.x)
 				Child_syn_need = true;
@@ -666,12 +685,12 @@ __global__ void ChildPath(Pathnode ** G_sequence_Queue, int * taskindex, int * p
 
 			if(blockIdx.x < G_Queue.blockcount)
 			{
-				if(inblocktindex<8)
+				if(inblocktindex < WARP_T*4)
 				{
-					for(int m = 0; m < Child_Init_S_queue[inblocktindex].count(); m++)
+					for(int m = 0; m < Child_Init_S_queue[Squeueindex][Squeueposindex].count(); m++)
 					{
-						G_Queue.G_queue[blockIdx.x][cpbackindex[inblocktindex]+m] = Child_Init_S_queue[inblocktindex].front();    //not sure about if the memory copy will work,need confirm.
-						Child_Init_S_queue[inblocktindex].pop();
+						G_Queue.G_queue[blockIdx.x][cpbackindex[inblocktindex]+m] = Child_Init_S_queue[Squeueindex][Squeueposindex].front();    //not sure about if the memory copy will work,need confirm.
+						Child_Init_S_queue[Squeueindex][Squeueposindex].pop();
 					}
 				}
 				if(inblocktindex == 0)
@@ -682,13 +701,13 @@ __global__ void ChildPath(Pathnode ** G_sequence_Queue, int * taskindex, int * p
 			}
 			else
 			{
-				if(inblocktindex<8)
+				if(inblocktindex < WARP_T*4)
 				{
-					for(int m = 0; m< Child_Init_S_queue[inblocktindex].count(); m++)
+					for(int m = 0; m< Child_Init_S_queue[Squeueindex][Squeueposindex].count(); m++)
 					{
 						int tmp = blockIdx.x % G_Queue.blockcount;
-						G_Queue.G_queue[m][Child_Queue_index[m]+cpbackindex[inblocktindex]+m] = Child_Init_S_queue[inblocktindex].front();
-						Child_Init_S_queue[inblocktindex].pop();
+						G_Queue.G_queue[m][Child_Queue_index[m]+cpbackindex[inblocktindex]+m] = Child_Init_S_queue[Squeueindex][Squeueposindex].front();
+						Child_Init_S_queue[Squeueindex][Squeueposindex].pop();
 					}
 				}
 				if(inblocktindex == 0)
@@ -701,7 +720,7 @@ __global__ void ChildPath(Pathnode ** G_sequence_Queue, int * taskindex, int * p
 			if(gridDim.x < BLOCK_SYN_THRESHOLD)
 				__gpu_blocks_simple_syn(gridDim.x);
 			else
-				__gpu_blocks_simple_syn(goalVal++, Arrayin, Arrayout);
+				__gpu_blocks_tree_syn(goalVal++, Arrayin, Arrayout);
 
 		}
 
@@ -758,16 +777,18 @@ __global__ void CudaUnion()
 
 
 //outtoing array: should add a -1 in the end of each array.
-string CudaGetCounterexample(int initial_t, int sccsize, int accsize, int totalsize, int startID, int * sccnodelist, int * acceptlist, int ** outgoingtransition, int outgoingwidth) //sccnodelist and acceptlist should be sorted for quick search
+int * CudaPath(int initial_t, int sccsize,  int totalsize, int startID, int * sccnodelist, int ** outgoingtransition, int outgoingwidth) //sccnodelist and acceptlist should be sorted for quick search
 {
 	int deviceCount;
 	int * G_path2scc, *H_path2scc;
-	int * G_path2acc, *H_path2acc;
+	//int * G_path2acc, *H_path2acc;
 	int ** G_outgoing;
 	int * G_sccnodelist;
-	int * G_acceptlist;
+	//int * G_acceptlist;
 	int i;
 	size_t acturalsize;
+
+	string returnresult;
 
 	SCCSIZE = sccsize;
 	TOTALSIZE = totalsize;
@@ -782,20 +803,17 @@ string CudaGetCounterexample(int initial_t, int sccsize, int accsize, int totals
 	H_path2scc = new int[totalsize-sccsize];
 	for(i = 0; i < totalsize-sccsize; i++)
 		H_path2scc = -1;
-	H_path2scc = new int[sccsize];
-	for(i = 0; i < totalsize-sccsize; i++)
-		H_path2scc = -1;
 
 	cudaMalloc((void**)&G_path2scc, sizeof(int)*(totalsize-sccsize));
-	cudaMalloc((void**)&G_path2acc, sizeof(int)*sccsize);
+	
 	cudaMalloc((void**)&G_sccnodelist, sizeof(int)*sccsize);
-	cudaMalloc((void**)&G_acceptlist, sizeof(int)*accsize); 
+	
 	cudaMallocPitch((void**)&G_outgoing, &acturalsize, sizeof(int)*outgoingwidth, totalsize);    //outgoing from pat should be a n*m
 
 	cudaMemcpy(G_path2scc,H_path2scc,sizeof(int)*(totalsize-sccsize),cudaMemcpyHostToDevice);
-	cudaMemcpy(G_path2acc,H_path2acc,sizeof(int)*(sccsize),cudaMemcpyHostToDevice);
+	
 	cudaMemcpy(G_sccnodelist,sccnodelist,sizeof(int)*sccsize, cudaMemcpyHostToDevice);
-	cudaMemcpy(G_acceptlist,acceptlist,sizeof(int)*sccsize, cudaMemcpyHostToDevice);
+	
 	cudaMemcpy2D(G_outgoing,acturalsize,outgoingtransition,sizeof(int)*outgoingwidth,outgoingwidth,totalsize,cudaMemcpyHostToDevice);
 	/*cudaStream_t counterexampleStream[2];
 	cudaStreamCreate(&counterexampleStream[0]);
@@ -809,8 +827,48 @@ string CudaGetCounterexample(int initial_t, int sccsize, int accsize, int totals
 	//cudaSetDevice();  optional to use
 	GPath<<<blockparameterp, gridparameterp, 32>>>(startID, G_sccnodelist, G_outgoing, G_path2scc);
 	cudaMemcpy(H_path2scc,G_path2scc, sizeof(int)*(totalsize-sccsize), cudaMemcpyDeviceToHost);
+		
+	return H_path2scc;
+}
 
-	GPath<<<blockparameterp,gridparameterp, 32>>>()
+int * CudaFindshortestpath(int initial_t, int sccsize, int accsize, int totalsize, int startID, int *acclist, int ** outgoingtransition, int outgoingwidth)
+{
+	int devicecount;
+	int * G_path2acc, *H_path2acc;
+	int ** G_outgoing;
+	int * G_acceptlist;
+	int i;
+	size_t acturalsize;
 
+	SCCSIZE = sccsize;
+	TASKTYPE = 2;
+	INITIAL_T = initial_t;
+
+	cudaGetDeviceCount(&devicecount);
+	if(devicecount == 0)
+		return NULL;
+
+	H_path2acc = new int[sccsize];
+	for(i = 0; i < sccsize; i++)
+		H_path2acc = -1;
+
+	cudaMalloc((void**)&G_path2acc, sizeof(int)*sccsize);
+	cudaMalloc((void**)&G_acceptlist, sizeof(int)*accsize); 
+	cudaMallocPitch((void**)&G_outgoing, &acturalsize, sizeof(int)*outgoingwidth, totalsize);  
+
+	cudaMemcpy(G_path2acc,H_path2acc,sizeof(int)*(sccsize),cudaMemcpyHostToDevice);
+	cudaMemcpy(G_acceptlist,acclist,sizeof(int)*sccsize, cudaMemcpyHostToDevice);
+	cudaMemcpy2D(G_outgoing,acturalsize,outgoingtransition,sizeof(int)*outgoingwidth,outgoingwidth,totalsize,cudaMemcpyHostToDevice);
+
+	dim3 blockparameterp(INITIAL_T,1,1);
+	dim3 gridparameterp(1,1,1);
+	//int gridparameter = 1; optional
+	Gquicksort<<<1,1>>>(&acclist, 0, accsize- 1);
+	cudaDeviceSynchronize();
+
+	GPath<<<blockparameterp,gridparameterp, 32>>>(startID, G_acceptlist, G_outgoing, G_path2acc);
+	cudaMemcpy(H_path2acc, G_path2acc, sizeof(int)*sccsize, cudaMemcpyDeviceToHost);
+
+	return H_path2acc;
 }
 
